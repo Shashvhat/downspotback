74f17c844a2bc0a87622579c55c7d50c394cc9e6
Shashvat Rawat <shashvatrawat729@gmail.com>
2023-09-17 19:31:11 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, render_template, redirect, url_for,jsonify
+from pytube import YouTube,Search
+
+app = Flask(__name__)
+
+# @app.route('/getVidInfo', methods=['GET', 'POST'])
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    # if request.method == 'POST':
+    # video_url = request.form['video_url']
+    try:
+        # Create a YouTube object
+        data = request.args.get('data')
+        yt = YouTube('https://www.youtube.com/watch?v=cFee50gHcIM&pp=ygURc3lzdGVtIHBlciBzeXN0ZW0%3D')
+        s = Search(data)
+        # print(s.results)
+        
+        for i in s.results:
+            print(i.title)
+
+        # Select the highest resolution stream
+        # stream = yt.streams.get_highest_resolution()
+        
+        # Download the video
+        stream = yt.streams.get_by_itag(313)
+        # stream.download()
+        # stream.audio_codec
+        
+        return jsonify('Downloaded successfully.')
+    except Exception as e:
+        return redirect(url_for('index', message=f'Error: {str(e)}'))
+
+    return 'Downloaded successfully.'
+
+if __name__ == '__main__':
+    app.run(debug=True)
