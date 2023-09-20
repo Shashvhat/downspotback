5ca36cc8d7b8d177034bf238243c6dd31a2afaec
Shashvat Rawat <shashvatrawat729@gmail.com>
2023-09-20 10:36:23 +0530

    commenting run

--- app.py
+++ app.py
@@ -33,5 +33,5 @@ def index():
 
     return 'Downloaded successfully.'
 
-if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5000)
+# if __name__ == '__main__':
+#     app.run(debug=False, host='0.0.0.0', port=5000)
